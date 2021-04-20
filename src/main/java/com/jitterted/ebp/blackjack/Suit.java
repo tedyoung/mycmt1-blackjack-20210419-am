bd4fb2d150b6cea7763dd4939a692e373a8b56db
Ted M. Young <tedyoung@gmail.com>
2021-04-20 12:08:01 -0700

    Lab #4: Fix Feature Envy by moving isRed to Suit

--- src/main/java/com/jitterted/ebp/blackjack/Suit.java
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -1,18 +1,24 @@
 package com.jitterted.ebp.blackjack;
 
 public enum Suit {
-  SPADES("♠"),
-  DIAMONDS("♦"),
-  HEARTS("♥"),
-  CLUBS("♣");
+  SPADES("♠", false),
+  DIAMONDS("♦", true),
+  HEARTS("♥", true),
+  CLUBS("♣", false);
 
   private final String symbol;
+  private final boolean isRed;
+
+  Suit(String symbol, boolean isRed) {
+    this.symbol = symbol;
+    this.isRed = isRed;
+  }
 
   public String symbol() {
     return symbol;
   }
 
-  Suit(String symbol) {
-    this.symbol = symbol;
+  public boolean isRed() {
+    return isRed;
   }
 }
