bf8a7a21e1b032f58ed5f8c05b68f4514e5db0e7
Ted M. Young <tedyoung@gmail.com>
2021-04-22 10:06:40 -0700

    Wallet isEmpty and addMoney behavior implemented.

---
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -0,0 +1,20 @@
+package com.jitterted.ebp.blackjack;
+
+public class Wallet {
+
+  private boolean isEmpty = true;
+  private int balance = 0;
+
+  public boolean isEmpty() {
+    return isEmpty;
+  }
+
+  public void addMoney(int amount) {
+    isEmpty = false;
+    balance += amount;
+  }
+
+  public int balance() {
+    return balance;
+  }
+}
