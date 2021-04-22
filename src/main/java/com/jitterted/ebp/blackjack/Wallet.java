39b2cc98beedeb8e05daf9f597f721e6f9505ce9
Ted M. Young <tedyoung@gmail.com>
2021-04-22 11:10:09 -0700

    Implemented bet method

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -22,4 +22,8 @@ public class Wallet {
   public int balance() {
     return balance;
   }
+
+  public void bet(int amount) {
+    balance -= amount;
+  }
 }
--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -70,6 +70,5 @@ public class WalletTest {
     assertThatThrownBy(() -> {
       wallet.addMoney(0);
     }).isInstanceOf(IllegalArgumentException.class);
-
   }
 }
